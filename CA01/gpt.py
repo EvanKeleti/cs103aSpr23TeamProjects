62cd4d0ae30f7a2a00100e61b48c79030096d0f2
simongoode <simongoode@brandeis.edu>
2023-03-17 11:15:51 -0400

    Update gpt.py
    added simon's request method

--- CA01/gpt.py
+++ CA01/gpt.py
@@ -44,6 +44,21 @@ class GPT():
 
         response = completion.choices[0].text
         return response
+    
+    #Simon Goode's request method
+    def comment_code(self,prompt):
+        ''' Generate GPT response that comments given code '''
+        completion = openai.Completion.create(
+            engine=self.model_engine,
+            prompt="write comments for this code:\n"+prompt,
+            max_tokens=1024,
+            n=1,
+            stop=None,
+            temperature=0.8,
+        )
+
+        response = completion.choices[0].text
+        return response
 
 if __name__=='__main__':
     '''
