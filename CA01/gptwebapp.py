4df44a40469e32b07653c4ada0840790d78846a0
nweiss25 <nweiss2002@gmail.com>
2023-03-18 11:40:36 -0400

    from main and my additions
    stuff that Lucas added to main, as well as my changes to Index, about and my methods

--- CA01/gptwebapp.py
+++ CA01/gptwebapp.py
@@ -11,7 +11,6 @@ On Mac
 % pip3 install flask
 % export APIKEY="......."  # in bash
 % python3 gptwebapp.py
-
 On Windows:
 % pip install openai
 % pip install flask
@@ -33,10 +32,46 @@ def index():
     ''' display a link to the general query page '''
     print('processing / route')
     return f'''
+        <h1>Team page</h1>
+        <a href="{url_for('Team')}">page with info about team members</a>
+        <h1>About page</h1>
+        <a href="{url_for('about')}">page with info about each GPT method</a>
         <h1>GPT Demo</h1>
         <a href="{url_for('gptdemo')}">Ask questions to GPT</a>
+        <h1>Shakesperean Sonnet</h1>
+        <a href="{url_for('shakeSonnet')}">Turn a prompt into a Shakespearean Sonnet</a>
+        <h1>Convert Python Code to MATLAB Code</h1>
+        <a href="{url_for('py_to_mat')}">Converts your python code into MATLAB code</a>
     '''
+@app.route('/Team')
+def Team():
+    return('''
+    <h1>Team Page</h1>
+    <b>Lucas Dia</b>
+    <p>I am a sophomore, I am on the Varsity XC and TF team. I am majoring in CS, ECON, and BUS. I created the Shakespearean Sonnet method, and the shakeSonnet method.
+    I also made the Team page, and about page. </p>
+    <br>
+    <b>Nathan Weiss</b>
+    <p>I'm a sophomore studying Applied Math and Computer Science here at Brandeis. I created generateMATLAB 
+    in gpt.py and py_to_mat in gptwebapp.py and contributed to the Index page and this page. </p>
+    <br>
+    '''
+    )
 
+@app.route('/about')
+def about():
+    return('''
+    <h1>About Page</h1>
+    <b>Shakesperean Sonnet Method</b>
+    <p>The Shakespearean Sonnet method takes the given prompt and asks GPT to write a shakespearean style sonnet using the prompt as inspiration. 
+    <br>
+    <br>
+    <b>Python to MATLAB Method</b>
+    <p>The Python to MATLAB method prompts the user to enter some python code that they want converted to MATLAB code. When that is input
+    and the form is passed, we add some text to the front of their code saying that we want this code converted to MATLAB before passing
+    to GPT. <p>
+    <br>
+    ''')
 
 @app.route('/gptdemo', methods=['GET', 'POST'])
 def gptdemo():
@@ -84,6 +119,8 @@ def py_to_mat():
         <div style="border:thin solid black">{answer}</div>
         <br>
         <a href={url_for('py_to_mat')}> Convert more code</a>
+        <br>
+        <a href={url_for('index')}> Index Page </a>
         '''
     else:
         return '''
@@ -91,8 +128,41 @@ def py_to_mat():
         Enter a block of Python code below that you would like GPT to MATLAB code
         <form method="post">
             <textarea name ="prompt"></textarea>
-            <p><input type=submit value="Get New Code">'''
+            <p><input type=submit value="Get New Code">
+        </form>
+        <a href={url_for('index')}> Index Page </a>
+        '''
+        
+
+
+@app.route('/shakeSonnet', methods=['GET', 'POST'])
+def shakeSonnet():
+    ''' handle a get request by sending a form 
+        and a post request by returning the GPT response
+    '''
+    if request.method == 'POST':
+        prompt = request.form['prompt']
+        answer = gptAPI.Shakesperean_Sonnet(prompt)
+        return f'''
+        <h1>Write a shakespearean sonnet using the prompt</h1>
+        <pre style="bgcolor:yellow">{prompt}</pre>
+        <hr>
+        Here is the answer in text mode:
+        <div style="border:thin solid black">{answer}</div>
+        Here is the answer in "pre" mode:
+        <pre style="border:thin solid black">{answer}</pre>
+        <a href={url_for('shakeSonnet')}> make another query</a>
+        '''
+    else:
+        return '''
+        <h1>GPT Shakesperean Sonnet APP</h1>
+        Enter your query below
+        <form method="post">
+            <textarea name="prompt"></textarea>
+            <p><input type=submit value="get response">
+        </form>
+        '''
 
 if __name__=='__main__':
     # run the code on port 5001, MacOS uses port 5000 for its own service :(
-    app.run(debug=True,port=5001)
+    app.run(debug=True,port=5002)
