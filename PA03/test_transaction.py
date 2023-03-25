12f4ef04b9d4a6491ee44089076191d0d77fec36
nweiss25 <nweiss2002@gmail.com>
2023-03-25 19:00:47 -0400

    update test

--- PA03/test_transaction.py
+++ PA03/test_transaction.py
@@ -5,23 +5,31 @@ from transaction import Transaction
 A = Transaction('transaction.db')
 
 def test_init():
-    '''tests the initialization of the db'''
+    '''tests the initialization of the db
+    Nathan Weiss'''
     assert A.showTransactions() == []
 
 def test_add():
     '''tests the add function
-    checks that items are added properly'''
-    list =A.addTransaction((1, 56.30, "grocery", "2023-01-01", "grocery shop"))
-    assert list[1]['item #'] == 1
-    assert list[1]['amount'] == 56.30
-    assert list[1]['category'] == 'grocery'
-    assert list[1]['date'] == '2023-01-01'
-    assert list[1]['description'] == 'grocery shop'
+    checks that items are added properly
+    Nathan Weiss'''
+    tr1 = {'item #':1, 'amount':56.30,'category':"test", 'date':"2023-01-01", 'description':'testing'}
+    A.addTransaction(tr1)
+    l = A.showTransactions()
+    assert l[len(l)-1]['item #'] == 1
+    assert l[len(l)-1]['amount'] == 56.30
+    assert l[len(l)-1]['category'] == 'test'
+    assert l[len(l)-1]['date'] == '2023-01-01'
+    assert l[len(l)-1]['description'] == 'testing'
 
-    list = A.addTransaction((4, 60.75, "textbook", "2023-03-25", "purchased textbook"))
-    assert list[2]['item #'] == 4
-    assert list[2]['amount'] == 60.75
-    assert list[2]['category'] == 'textbook'
-    assert list[2]['date'] == '2023-03-25'
-    assert list[2]['description'] == 'purchased textbook'
-
+    tr2 = {'item #':4, 'amount':60.75,'category':"textbook", 'date':"2023-03-25", 'description':'purchased textbook'}
+    A.addTransaction(tr2)
+    l = A.showTransactions()
+    assert l[len(l)-1]['item #'] == 4
+    assert l[len(l)-1]['amount'] == 60.75
+    assert l[len(l)-1]['category'] == 'textbook'
+    assert l[len(l)-1]['date'] == '2023-03-25'
+    assert l[len(l)-1]['description'] == 'purchased textbook'
+def test_show():
+    '''tests that showTransactions shows all transactions'''
+    assert A.showTransactions() == [{'item #': 1, 'amount': 56.3, 'category': 'grocery', 'date': '2023-01-01', 'description': 'grocery shop'}, {'item #': 4, 'amount': 60.75, 'category': 'textbook', 'date': '2023-03-25', 'description': 'purchased textbook'}]
