c313ac3daa0e91804d387627883822b77dff88e0
Lucas-Dia-99 <lucas.t.dia@gmail.com>
2023-03-25 17:41:24 -0400

    Update tracker.py

--- PA03/tracker.py
+++ PA03/tracker.py
@@ -5,7 +5,7 @@ def print_usage():
     ''' print an explanation of how to use this command '''
     print('''usage:
             transaction show
-            transaction add name description
-            transaction delete item_id
+            transaction add transaction
+            transaction delete item #
             '''
             )
