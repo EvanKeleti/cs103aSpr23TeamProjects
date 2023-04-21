7a6f551c01e93aa9b4ce90efb00fa8d023be7d43
Lucas-Dia-99 <lucas.t.dia@gmail.com>
2023-04-20 21:38:03 -0400

    created transaction route

--- PA04/firstapp/routes/transaction.js
+++ PA04/firstapp/routes/transaction.js
@@ -4,4 +4,58 @@
 const express = require('express');
 const router = express.Router();
 const ToDoItem = require('../models/TransactionItem')
-const User = require('../models/User')
+const User = require('../models/User');
+const TransactionItem = require('../models/TransactionItem');
+
+isLoggedIn = (req,res,next) => {
+    if (res.locals.loggedIn) {
+      next()
+    } else {
+      res.redirect('/login')
+    }
+  }
+
+// get the value associated to the key
+router.get('/transaction/',
+  isLoggedIn,
+  async (req, res, next) => {
+      const show = req.query.show
+      const completed = show=='completed'
+      let items=[]
+      if (show) { // show is completed or transaction, so just show some items
+        items = 
+          await TransactionItem.find({userId:req.user._id, completed})
+                        .sort({completed:1,priority:1,createdAt:1})
+      }else {  // show is null, so show all of the items
+        items = 
+          await TransactionItem.find({userId:req.user._id})
+                        .sort({completed:1,priority:1,createdAt:1})
+
+      }
+            res.render('transactions',{items,show,completed});
+});
+
+/* add the value in the body to the list associated to the key */
+router.post('/transaction',
+  isLoggedIn,
+  async (req, res, next) => {
+      const todo = new TransactionItem(
+        {item:req.body.item,
+         createdAt: new Date(),
+         completed: false,
+         priority: parseInt(req.body.priority),
+         userId: req.user._id
+        })
+      await transaction.save();
+      res.redirect('/transaction')
+});
+
+router.get('/transaction/remove/:itemId',
+  isLoggedIn,
+  async (req, res, next) => {
+      console.log("inside /transaction/remove/:itemId")
+      await TransactionItem.deleteOne({_id:req.params.itemId});
+      res.redirect('/transaction')
+});
+
+module.exports = router;
